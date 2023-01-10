8dc784c0dc7b4767f546d1e6aadb866d117d74f9
victorHL99 <victorlessa.dev@gmail.com>
2023-01-10 12:17:27 -0300

    feat: :sparkles: create program for render one pyramid that depends of number inserted

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import javax.swing.JOptionPane;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int number = Integer.parseInt(JOptionPane.showInputDialog("Enter a number: "));
+
+        for (int i = 0; i <= number; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
     }
+
 }
